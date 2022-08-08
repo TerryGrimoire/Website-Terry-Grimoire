ec767e97f85bdb839bc42b7f91b0a0fb6be39c6f
Terry Grimoire <terry.grimoire@gmail.com>
2022-08-08 17:09:43 +0200

    adding the checkpoints on the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,44 @@
+import React from "react";
+import coche from "../assets/coche.png";
+
 export default function Home() {
   return (
     <main className="main_container">
       <article className="flex-col article_home">
         <h2 className="first_h2_home">
-          Le monde change, votre entreprise devrait aussi
+          Le monde change, votre entreprise devrait aussi.
         </h2>
-        <p>NOKIA</p>
-        <p>AOL</p>
-        <p>KODAK</p>
-        <p>MAMMOUTH</p>
-        <p>POLAROID</p>
-        <p>BAMBOULA</p>
-        <p>COMPAQ</p>
-        <p>
-          Toutes ces entreprises ont disparues ou ont perdues d'énormes part de
-          marché parce qu'elles n'ont pas su s'adapater et vivre avec leur
-          temps.
-        </p>
 
-        <h2 className="second_h2_home">
-          Pourquoi et comment numériser son entreprise ?{" "}
-        </h2>
-        <p>NOKIA</p>
-        <p>AOL</p>
-        <p>KODAK</p>
-        <p>MAMMOUTH</p>
-        <p>POLAROID</p>
-        <p>BAMBOULA</p>
-        <p>COMPAQ</p>
-        <p>
-          Toutes ces entreprises ont disparues ou ont perdues d'énormes part de
-          marché parce qu'elles n'ont pas su s'adapater et vivre avec leur
-          temps.
-        </p>
+        <div className="flex align-center justify-between">
+          <p>Nous contacter</p>
+          <button type="button" className="button_style blue">
+            Découvrir nos offres
+          </button>
+        </div>
+
+        <div>
+          <div className="flex align-center justify-evenly">
+            <img src={coche} alt="coche" />
+            <p className="p_home">
+              <span>Rapide et efficace.</span> Un site internet possible en
+              moins d'une semaine.
+            </p>
+          </div>
+          <div className="flex align-center justify-evenly">
+            <img src={coche} alt="coche" />
+            <p className="p_home">
+              <span>Une offre complète.</span> Le nécessaire pour faire grandir
+              votre entreprise.
+            </p>
+          </div>
+          <div className="flex align-center justify-evenly">
+            <img src={coche} alt="coche" />
+            <p className="p_home">
+              <span>Prix transparents.</span> Une tarification claire et précise
+              dès le début.
+            </p>
+          </div>
+        </div>
       </article>
     </main>
   );

a88927806521ec2e300a84a5099d7c0b267f1da4
Terry Grimoire <terry.grimoire@gmail.com>
2022-08-08 21:17:22 +0200

    adding the map for the expertiseData and Expertise section + the 2 other sections tarifications and former clients

---
+++ src/datas/ExpertiseData.js
@@ -0,0 +1,40 @@
+const ExpertiseData = [
+  {
+    id: 1,
+    link: "/Services",
+    title: "Développement web",
+    img: "https://media2.giphy.com/media/qgQUggAC3Pfv687qPC/giphy.gif?cid=ecf05e47iv7d6wz6o6xwo2qep88pci80l3py1fz3zrmk497e&rid=giphy.gif&ct=g",
+    alt: "developpeur en train de coder sur son ordinateur",
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio dicta molestias hic distinctio nam ullam vero blanditiis alias? Excepturi voluptatibus placeat accusamus blanditiis voluptas, vero beatae velit animi eveniet nulla quis nesciunt consequuntur amet nisi dolores unde incidunt omnis maiores sequi eaque dolorem. Accusantium perferendis, dolor dolore neque alias sed.",
+  },
+  {
+    id: 2,
+    link: "/Services",
+    title: "Analyse de données",
+    img: "https://media3.giphy.com/media/3oKIPEqDGUULpEU0aQ/giphy.gif?cid=ecf05e47rf99mjtr9gizkyu7zgm94cfgh4yrga2em6nw6ped&rid=giphy.gif&ct=g",
+    alt: "main qui ouvre et referme un dashboard sur lequel on voit des analyses et graphiques",
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio dicta molestias hic distinctio nam ullam vero blanditiis alias? Excepturi voluptatibus placeat accusamus blanditiis voluptas, vero beatae velit animi eveniet nulla quis nesciunt consequuntur amet nisi dolores unde incidunt omnis maiores sequi eaque dolorem. Accusantium perferendis, dolor dolore neque alias sed.",
+  },
+  {
+    id: 1,
+    link: "/Services",
+    title: "Digital Marketing",
+    img: "https://media3.giphy.com/media/jQmn1Dkw55R3cjm3eC/giphy.gif?cid=ecf05e47mx9xj3iataiewm1kemtxnfdqa4pr3pr5zzea17wl&rid=giphy.gif&ct=g",
+    alt: "Mains dans lesquelles ont voit des portables qui sont sur des réseaux sociaux",
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio dicta molestias hic distinctio nam ullam vero blanditiis alias? Excepturi voluptatibus placeat accusamus blanditiis voluptas, vero beatae velit animi eveniet nulla quis nesciunt consequuntur amet nisi dolores unde incidunt omnis maiores sequi eaque dolorem. Accusantium perferendis, dolor dolore neque alias sed.",
+  },
+  {
+    id: 1,
+    link: "/Services",
+    title: "Négociation auprès des partenaires bancaires",
+    img: "https://media2.giphy.com/media/S7K9EsqHvTGaOGmfKd/giphy.gif?cid=ecf05e47wlf4xvdq1s9qaxt6z5r4waglzdsdpxowlb3iauwb&rid=giphy.gif&ct=g",
+    alt: "nombreux billets verts qui volent dans le ciel",
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio dicta molestias hic distinctio nam ullam vero blanditiis alias? Excepturi voluptatibus placeat accusamus blanditiis voluptas, vero beatae velit animi eveniet nulla quis nesciunt consequuntur amet nisi dolores unde incidunt omnis maiores sequi eaque dolorem. Accusantium perferendis, dolor dolore neque alias sed.",
+  },
+];
+
+export default ExpertiseData;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,45 +1,79 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import coche from "../assets/coche.png";
+import expertiseData from "../datas/ExpertiseData";
 
 export default function Home() {
   return (
     <main className="main_container">
-      <article className="flex-col article_home">
-        <h2 className="first_h2_home">
-          Le monde change, votre entreprise devrait aussi.
+      <h2 className="first_h2_home">
+        Le monde change, votre entreprise devrait aussi.
+      </h2>
+
+      <div className="flex align-center justify-between">
+        <p>Nous contacter</p>
+        <button type="button" className="button_style blue">
+          Découvrir nos offres
+        </button>
+      </div>
+
+      <section>
+        <div className="flex align-center justify-evenly">
+          <img src={coche} alt="coche" className="check" />
+          <p className="p_home">
+            <span>Rapide et efficace.</span> Un site internet possible en moins
+            d'une semaine.
+          </p>
+        </div>
+        <div className="flex align-center justify-evenly">
+          <img src={coche} alt="coche" className="check" />
+          <p className="p_home">
+            <span>Une offre complète.</span> Site web, analyse de données,
+            réseaux sociaux et financiers, etc.
+          </p>
+        </div>
+        <div className="flex align-center justify-evenly">
+          <img src={coche} alt="coche" className="check" />
+          <p className="p_home">
+            <span>Prix transparents.</span> Une tarification claire et précise
+            dès le début.
+          </p>
+        </div>
+      </section>
+      <section>
+        <h2 className="second_h2_home">
+          Notre expertise vous fait grandir sereinement et sainement
         </h2>
 
-        <div className="flex align-center justify-between">
-          <p>Nous contacter</p>
-          <button type="button" className="button_style blue">
-            Découvrir nos offres
-          </button>
-        </div>
+        {expertiseData.map((expertise) => (
+          <article className="flex-col align-center article_home">
+            <h3>{expertise.title}</h3>
+            <img src={expertise.img} alt={expertise.alt} />
+            <p>{expertise.content}</p>
+            <Link to={expertise.link}>
+              <button type="button" className="button_style margin1r0">
+                En savoir plus
+              </button>
+            </Link>
+          </article>
+        ))}
+      </section>
 
-        <div>
-          <div className="flex align-center justify-evenly">
-            <img src={coche} alt="coche" />
-            <p className="p_home">
-              <span>Rapide et efficace.</span> Un site internet possible en
-              moins d'une semaine.
-            </p>
-          </div>
-          <div className="flex align-center justify-evenly">
-            <img src={coche} alt="coche" />
-            <p className="p_home">
-              <span>Une offre complète.</span> Le nécessaire pour faire grandir
-              votre entreprise.
-            </p>
-          </div>
-          <div className="flex align-center justify-evenly">
-            <img src={coche} alt="coche" />
-            <p className="p_home">
-              <span>Prix transparents.</span> Une tarification claire et précise
-              dès le début.
-            </p>
-          </div>
-        </div>
-      </article>
+      <section>
+        <h2 className="first_h2_home">
+          Des tarifs transparents, sans surprise.
+        </h2>
+      </section>
+      <section>
+        <h2 className="second_h2_home">Ils nous ont fait confiance</h2>
+        <p>Le Montecito</p>
+        <p>We Rev'Art</p>
+        <p>La plaisir Bistrot Queer</p>
+        <p>Charles Girouard</p>
+        <p>Le collectif Bubble Gum</p>
+        <p>Le cercle rouge</p>
+        <p>Les sirs carmes</p>
+      </section>
     </main>
   );
 }

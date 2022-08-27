90deb3f584a97b2fab778212df49705e110305f4
Terry Grimoire <terry.grimoire@gmail.com>
2022-08-27 12:24:26 +0200

    creating the clients page and adding the information to the data file partenairesData

---
+++ src/datas/ClientsData.js
@@ -0,0 +1,32 @@
+const clientsData = [
+  {
+    id: 1,
+    title: "Charles Girouard",
+    description: "",
+    img: "",
+    alt: "",
+  },
+  {
+    id: 2,
+    title: "",
+    description: "",
+    img: "",
+    alt: "",
+  },
+  {
+    id: 1,
+    title: "",
+    description: "",
+    img: "",
+    alt: "",
+  },
+  {
+    id: 1,
+    title: "",
+    description: "",
+    img: "",
+    alt: "",
+  },
+];
+
+export default clientsData;

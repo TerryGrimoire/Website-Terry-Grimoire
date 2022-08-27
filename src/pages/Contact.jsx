6f42299f23f88ec38a9a9f732470b2c763689aa8
Terry Grimoire <terry.grimoire@gmail.com>
2022-08-27 10:51:23 +0200

    creating the contact page in mobile version - form ok but not functional for now + calender ok and functional

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "../components/Form";
 
 function Contact() {
-  return <div>Contact</div>;
+  const [rdv, setRdv] = useState(true);
+  return (
+    <div className="flex flex-col align-center">
+      <h2>Nous joindre</h2>
+      <section>
+        <div className="flex justify-center margin1r0 ">
+          <button
+            type="button"
+            className="button_style margin0r1r"
+            onClick={() => setRdv(true)}
+          >
+            Prendre un rdv
+          </button>
+          <button
+            type="button"
+            className="button_style margin0r1r"
+            onClick={() => setRdv(false)}
+          >
+            Envoyer un email
+          </button>
+        </div>
+        {rdv ? (
+          <iframe
+            src="https://calendly.com/terry-grimoire/30min?month=2022-08"
+            frameBorder="0"
+            title="calendly terry grimoire"
+          />
+        ) : (
+          <Form />
+        )}
+      </section>
+    </div>
+  );
 }
 
 export default Contact;

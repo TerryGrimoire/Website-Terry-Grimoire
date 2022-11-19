a37406b49a9cd854983bf84d348d3d0491e94c70
Terry Grimoire <terry.grimoire@gmail.com>
2022-11-19 12:21:29 +0400

    adding blog1 article

---
+++ src/datas/blogData.js
@@ -0,0 +1,35 @@
+import terry from "../assets/terry_grimoire_mobile.jpg";
+
+const blogData = [
+  {
+    id: 1,
+    title:
+      "Le Grimoire Numérique : Qui se cache derrière le Grimoire Numérique ?",
+    src: terry,
+    alt: "portrait Terry Grimoire",
+    description:
+      "Je suis Terry Grimoire, jeune réunionnais de 28 ans, troisième membre d’une famille nombreuse et modeste originaire du Port. Depuis mon plus jeune âge, j’ai appris à m’en sortir par mes propres moyens, dès mes 14 ans, j’ai travaillé dans les champs pour financer mes premiers téléphones portables. J’ai sacrifié de nombreux ... ",
+    catagory: "GN",
+    link: "/blog/1",
+  },
+  {
+    id: 2,
+    title: "Tout savoir sur le kap numérik (anciennement chèque numérique)",
+    src: "https://regionreunion.com/IMG/jpg/kapnume_rik1000x650-2.jpg",
+    alt: "",
+    description: "",
+    catagory: "Kap numérik",
+    link: "/blog/2",
+  },
+  {
+    id: 3,
+    title: "Le Grimoire Numérique vous aide à monter votre dossier kap numérik",
+    src: "https://cdn.pixabay.com/photo/2018/04/05/15/53/office-work-3293203_960_720.jpg",
+    alt: "",
+    description: "",
+    catagory: "kap numérik",
+    link: "/blog/3",
+  },
+];
+
+export default blogData;
---
+++ src/pages/Blog.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import Helmet from "react-helmet";
+import blogData from "../datas/blogData";
+
+function Services() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <section className="blog">
+      <Helmet>
+        <title> Grimoire Numérique | Blog </title>
+        <link rel="canonical" href="https://grimoire-numerique.re/Blog" />
+        <meta
+          name="description"
+          content="Notre blog vous permet d'avoir des informations sur divers sujets tels que le kap numérik, les membres du Grimoire Numérique, la situation numérique réunionnaise."
+        />
+      </Helmet>
+      <h1 className="h1">Blog</h1>
+      <div className="blog_container">
+        {blogData.map((blog) => (
+          <Link to={`/Blog/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <img src={blog.src} alt={blog.alt} />
+            <p>{blog.description}</p>
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Services;
